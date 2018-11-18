22c1cc41784d8c69406be6b1ea841e37dd54adf7
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-11-18 09:18:53 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import math
+import time
+print('软件加载成功')
+time.sleep(0.5)
+print('作者：Mr.yan')
+time.sleep(0.5)
+print('使用规则：')
+print('         1.禁止输入<0或>100000000的数')
+print('         1.禁止输入字母')
+time.sleep(0.5)
+var=1
+while var == 1 :
+    print('请输入一个数字，我们将计算它的圆面积：')
+    time.sleep(1)
+    a=3.14
+    r=int(input())
+    if 10000000>= r > 0:
+        b=r*r
+        print('获得半径为：',r)
+        time.sleep(1)
+        print('正在计算')
+        time.sleep(3)
+        print('圆的面积为:',b*a)
+        time.sleep(0.5)
+        print('计算完成')
+    else:
+        print('计算失败')
