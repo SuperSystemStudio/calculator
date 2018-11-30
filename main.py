e599e9fd1c9e43c54c0e6b412605fda35bf84934
Mryan <40586009+Mryan2005@users.noreply.github.com>
2018-11-30 20:12:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ InspectionCenter.main
 # Global variable list
 π=3.14
 # Inspection Center
-print('检查中心正在检查')
+print('[info]检查中心正在检查')
 time.sleep(0.5)
-print('检查.travis.yml是否存在')
+print('[info]检查.travis.yml是否存在')
 time.sleep(0.5)
 if os.path.exists('.travis.yml'): # 如果文件存在
     # 删除文件，可使用以下两种方法。
-    print('检查到.travis.yml')
+    print('[info]检查到.travis.yml')
     time.sleep(0.5)
-    print('正在处理')
+    print('[info]正在处理')
     time.sleep(0.5)
     os.remove('.travis.yml') # 则删除
     time.sleep(0.5)
-    print('处理完成')
+    print('[info]处理完成')
     #os.unlink(my_file)
 else:
     print('no such file .travis.yml')
